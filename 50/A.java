23ec7ee5dc51fc91d0c92894fe99e76b1ad82410
ClaretWheel1481 <245922575@qq.com>
2023-02-06 18:51:47 +0800

    50A Domino piling

--- 50/A.java
+++ 50/A.java
@@ -1,6 +1,6 @@
     import java.util.*;
      
-    public class main{
+    public class A{
         public static void main(String[] args){
             Scanner in = new Scanner(System.in);
             int M = in.nextInt();
