9b6180375e03149ef862edfec542555446825098
ClaretWheel1481 <245922575@qq.com>
2023-02-06 18:51:20 +0800

    158A Domino piling

---
+++ 50/A.java
@@ -0,0 +1,11 @@
+    import java.util.*;
+     
+    public class main{
+        public static void main(String[] args){
+            Scanner in = new Scanner(System.in);
+            int M = in.nextInt();
+            int N = in.nextInt();
+            int domino = 2;
+            System.out.println(M*N / domino);
+        }
+    }
