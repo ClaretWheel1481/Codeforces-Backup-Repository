533055fff93618f015c1d3d8ae49a201cc01715f
ClaretWheel1481 <245922575@qq.com>
2023-03-03 20:49:35 +0800

    1676B Equal Candies

---
+++ Java/1676/B.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class A {
+	
+	public static int solve(int []nums) {
+		Arrays.sort(nums);
+		int sum = 0;
+		for(int i  = 0;i<nums.length;i++) {
+			sum+=nums[i]-nums[0];
+		}
+		return sum;
+	}
+	
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int t = in.nextInt();
+		for(int i = 0;i<t;i++) {
+			int n = in.nextInt();
+			int []nums = new int[n];
+			for(int j = 0;j<n;j++) {
+				nums[j] = in.nextInt();
+			}
+			System.out.println(solve(nums));
+		}
+	}
+}
