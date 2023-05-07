6fe853e62e0b0ba50d71bb780ec59dd09fcf979f
ClaretWheel1481 <245922575@qq.com>
2023-05-07 23:02:03 +0800

    1829B Blank Space

--- C++/1829/B.cpp
+++ C++/1829/B.cpp
@@ -2,13 +2,9 @@
 
 using namespace std;
 
-int main()
+void blankspaces()
 {
-    int t;
-    cin >>t;
-    while(t--)
-    {
-        int al,blankspace = 0,longestblankspace = 0;
+    int al,blankspace = 0,longestblankspace = 0;
         cin >> al;
         vector<int> a(al);
         for(int i = 0;i<al;i++)
@@ -18,7 +14,14 @@ int main()
             else if(a[i] != 0) blankspace = 0;
             longestblankspace = max(blankspace,longestblankspace);
         }
-        cout << longestblankspace << endl;
-    }
+    cout << longestblankspace << endl;
+    return ;
+}
+
+int main()
+{
+    int t;
+    cin >>t;
+    while(t--)blankspaces();
     return 0;
 }
